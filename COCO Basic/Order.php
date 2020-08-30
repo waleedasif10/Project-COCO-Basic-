853b985cd56093b65bf6c37d0027a9b335f523b6
Muhammad Waleed Asif <70438851+waleedasif10@users.noreply.github.com>
2020-08-31 00:42:34 +0500

    Add files via upload

---
+++ COCO Basic/Order.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+echo "My name is:". $_GET['text1'].'<br>';
+echo "My Designation is:". $_GET['r1'].'<br>';
+echo "My ID is:".$_GET['password1'].'<br>';
+echo "My Age is:".$_GET['selage'].'<br>';
+echo "My Gender is:".$_GET['e1'].'<br>';
+echo "My Address is :".$_GET['txtadd'].'<br>';  
+?>
+
+</body>
+</html>
---
+++ COCO Basic/welcome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+Welcome <?php echo $_POST["firstname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+Thank You for you Response
+
+</body>
+</html>
